f2624db6abecc439ed816003628028efa7b8277b
khyan22 <khyanalvarez@gmail.com>
2022-05-19 03:10:50 -0400

    added routes to server.js and fetch request to animals.js and index.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001
 const { animals } = require('./data/animals')
 const app = express()
 
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
@@ -87,6 +88,22 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404)
     }
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'))
+})
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'))
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './'))
 })
 
 app.post('/api/animals', (req, res) => {
